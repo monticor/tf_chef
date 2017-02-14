ce4bd477fd18138ea51b255d00ffaef99e859ba3
monticor <ben.gilbert@lcloudtp.com>
2017-02-14 02:14:28 -0500

    config updates

--- main.tf
+++ main.tf
@@ -227,7 +227,7 @@ resource "null_resource" "chef_chef-server" {
     run_list        = ["recipe[system::default]","recipe[chef-client::default]","recipe[chef-client::config]","recipe[chef-client::cron]","recipe[chef-client::delete_validation]","recipe[chef-server::default]","recipe[chef-server::addons]"]
     server_url      = "https://${aws_instance.chef-server.tags.Name}/organizations/${var.chef_org["short"]}"
     skip_install    = true
-#    user_name       = "${var.chef_user["username"]}"
+    user_name       = "${var.chef_user["username"]}"
 #   user_key        = "${file(".chef/user.pem")}"
   }
 }
